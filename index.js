6f5ba2a90b6434fe24827615d43ebd88e90c470b
Maxim Sinelnikov <maxim@sinelnikov.info>
2019-05-01 18:54:41 +0200

    beer details

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   axios
-    .get(process.env.API_URL, {
+    .get(process.env.API_URL + "beers/", {
+      params: {
+        key: process.env.API_KEY,
+        p: req.headers.pagenr
+      }
+    })
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get("/detailsbyid", (req, res) => {
+  axios
+    .get(process.env.API_URL + "beer/" + `${req.headers.beerid}`, {
       params: {
         key: process.env.API_KEY
       }
